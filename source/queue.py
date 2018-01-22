b2043ac49b66bd0a640961189a5090c30d0fbd08
Ansel Bridgewater <iamansel@gmail.com>
2018-01-22 00:49:25 -0800

    Finished LinkedQueue class

--- source/queue.py
+++ source/queue.py
@@ -21,27 +21,39 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        # Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        # Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        # Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        # Return front item, if any
+        if self.is_empty():
+            return None
+        return self.list.get_at_index(0)
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        # Remove and return front item, if any
+        if self.is_empty():
+            raise ValueError('Cannot dequeue on an empty queue')
+
+        front = self.list.get_at_index(0)
+        self.list.delete(front)
+        return front
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
@@ -76,7 +88,7 @@ class ArrayQueue(object):
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        # Return front item, if any
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
