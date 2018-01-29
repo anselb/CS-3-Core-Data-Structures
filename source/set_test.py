d43ef72caddbcf58b212ea2bb0e5baaf87966799
Ansel Bridgewater <iamansel@gmail.com>
2018-01-28 17:50:42 -0800

    Initialized set_test file

---
+++ source/set_test.py
@@ -0,0 +1,14 @@
+#!python
+
+from set import Set
+import unittest
+# Python 2 and 3 compatibility: unittest module renamed this assertion method
+if not hasattr(unittest.TestCase, 'assertCountEqual'):
+    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+
+
+class SetTest(unittest.TestCase):
+
+    def test_init(self):
+        st = Set(['a', 'b', 'c'])
+        assert st.size == 3
