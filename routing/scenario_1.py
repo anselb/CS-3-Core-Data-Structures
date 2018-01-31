c9290dd34cffdbc0b97eb2a141cdd53e76fe02ad
Ansel Bridgewater <iamansel@gmail.com>
2018-01-31 15:08:40 -0800

    Created quick, but dirty, script for finding cost of one number

---
+++ routing/scenario_1.py
@@ -0,0 +1,18 @@
+"""You have a carrier route list with 100,000 (100K) entries (in arbitrary
+order) and a single phone number. How quickly can you find the cost of calling
+this number?"""
+
+
+import sys
+sys.path.append('../source/')
+
+from strings import find_all_indexes
+
+
+with open('data/route-costs-106000.txt', 'r') as f:
+    data = f.read()
+    number = '+3346182'
+    indexes = find_all_indexes(data, number)
+    index = indexes[0]
+    test = data[index + len(number) + 1:index + len(number) + 5]
+print(test)
