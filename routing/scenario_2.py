58efd6710d56902340bcc8b901aa219d78f90aaa
Ansel Bridgewater <iamansel@gmail.com>
2018-02-05 16:00:58 -0800

    Added method to write costs to file

--- routing/scenario_2.py
+++ routing/scenario_2.py
@@ -29,14 +29,28 @@ def search_hashtable(costs_table, phone_number):
     while phone_number != '+':
         if costs_table.get(phone_number) is None:
             phone_number = phone_number[:-1]
-            print(phone_number)
         else:
             return costs_table.get(phone_number)
 
     return route_cost
 
+def write_costs_to_file(costs_table, file_to_read):
+    """Given a dictionary of phone numbers and costs and the name of a file
+    with phone numbers, write the costs of calling each of the given numbers to
+    the file."""
+    numbers = file_to_read[19:-1 - 3]
+    output_file = "test_files/route-costs-" + numbers + ".txt"
+    with open(file_to_read, 'r') as f:
+        for line in f:
+            cost = search_hashtable(costs_table, line)
+            with open(output_file, 'a') as g:
+                g.write(line.strip('\n') + "," + str(cost) + '\n')
 
 if __name__ == '__main__':
-    costs_table = read_to_hashtable('data/route-costs-106000.txt')
-    print(search_hashtable(costs_table, '+3314087756'))
+    costs_table = read_to_hashtable('data/route-costs-10000000.txt')
+    write_costs_to_file(costs_table, 'data/phone-numbers-1000.txt')
+    
+    # costs_table = read_to_hashtable('data/route-costs-106000.txt')
+    # print(search_hashtable(costs_table, '+3314087756'))
+
     # print(read_to_hashtable('data/route-costs-10.txt'))
