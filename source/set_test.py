53d10108da55092ca36365cf0ab33b930bc72b70
Ansel Bridgewater <iamansel@gmail.com>
2018-02-07 15:07:52 -0800

    Finished tests for 3 methods in Set class

--- source/set_test.py
+++ source/set_test.py
@@ -12,3 +12,71 @@ class SetTest(unittest.TestCase):
     def test_init(self):
         st = Set(['a', 'b', 'c'])
         assert st.size == 3
+
+    def test_contains(self):
+        st = Set(['a', 'b', 'c'])
+        assert st.contains('a') is True
+        assert st.contains('b') is True
+        assert st.contains('c') is True
+
+        assert st.contains('d') is False
+        assert st.contains('e') is False
+        assert st.contains('f') is False
+
+    def test_add(self):
+        st = Set()
+        st.add('a')
+        assert st.size == 1
+        assert st.contains('a') is True
+
+        st.add('b')
+        assert st.size == 2
+        assert st.contains('a') is True
+        assert st.contains('b') is True
+
+        st.add('c')
+        assert st.size == 3
+        assert st.contains('a') is True
+        assert st.contains('b') is True
+        assert st.contains('c') is True
+
+    def test_removie(self):
+        st = Set(['a', 'b', 'c'])
+        assert st.size == 3
+        assert st.contains('a') is True
+        assert st.contains('b') is True
+        assert st.contains('c') is True
+
+        st.remove('a')
+        assert st.size == 2
+        assert st.contains('a') is False
+        assert st.contains('b') is True
+        assert st.contains('c') is True
+
+        st.remove('b')
+        assert st.size == 1
+        assert st.contains('a') is False
+        assert st.contains('b') is False
+        assert st.contains('c') is True
+
+        st.remove('c')
+        assert st.size == 0
+        assert st.contains('a') is False
+        assert st.contains('b') is False
+        assert st.contains('c') is False
+
+    def test_union(self):
+        st = Set()
+        pass
+
+    def test_intersection(self):
+        st = Set()
+        pass
+
+    def test_difference(self):
+        st = Set()
+        pass
+
+    def test_is_subset(self):
+        st = Set()
+        pass
